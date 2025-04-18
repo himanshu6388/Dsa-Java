b0756f723fd0745fe0c06228ea459655b79cda19
himanshu6388 <himanshu219181@gmail.com>
2025-04-18 09:13:20 +0530

    second Commit

--- Day01/JavaBasic.java
+++ Day01/JavaBasic.java
@@ -2,9 +2,22 @@ package Day01;
 
 public class JavaBasic {
     public static void main(String args[]){
-      int a =10;
-      int b= 5;
-      System.out.println("a:"+ a);
-      System.out.println("b:"+b);
+    //   int a =10;
+    //   int b= 5;
+    //   System.out.println("a:"+ a);
+    //   System.out.println("b:"+b);
+
+    byte b = 8;
+    System.out.println(b);
+    char ch = 'a';
+    System.out.println(ch);
+    boolean var = true;
+    System.out.println(var);
+    float price = (float) 10.5;
+    System.out.println(price);
+    int number = 25;
+    System.out.println(number);
+    short n = 4;
+    System.out.println(n);
     }
 }
---
+++ Day01/Sum.java
@@ -0,0 +1,16 @@
+package Day01;
+
+public class Sum {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+
+        // -> Single line comment
+
+        /* ->
+         Multiline  Comment
+         */
+    }
+}
