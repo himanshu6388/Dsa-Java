1407560d9a373a208c161fd9c9a59650efb83065
himanshu6388 <himanshu219181@gmail.com>
2025-04-22 07:58:36 +0530

    updated

--- Day01/typecast.java
+++ Day01/typecast.java
@@ -27,12 +27,16 @@ public class typecast {
         // System.out.println((int)(a));
         // System.out.println(b-a);
 
-        int  a = 10;
-        float b = 20.35f;
-        long c = 25;
-        double d = 30;
-        double ans = a + b + c+d;
-        System.out.println(ans);
+        // int  a = 10;
+        // float b = 20.35f;
+        // long c = 25;
+        // double d = 30;
+        // double ans = a + b + c+d;
+        // System.out.println(ans);
+
+        byte b= 5;
+        byte a = (byte) (b*2);
+        System.out.println(a);
 
     }
     
