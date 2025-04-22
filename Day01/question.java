09913d2760cd5eb1c3d1d8c0a60040cb4b9fedb5
himanshu6388 <himanshu219181@gmail.com>
2025-04-22 08:16:00 +0530

    updated

---
+++ Day01/question.java
@@ -0,0 +1,41 @@
+package Day01;
+import java.util.Scanner;
+
+public class question {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // Question 1 Calculate Average
+        // System.out.println("Enter Number a:");
+        // int a = sc.nextInt();
+        // System.out.println("Enter Number b:");
+        // int b = sc.nextInt();
+        // System.out.println("Enter Number c:");
+        // int c = sc.nextInt();
+        // int sum = a + b + c;
+        // int avg = sum /3;
+        // System.out.println("Aberage is:" + avg);
+
+
+        // Question 2 Calculate area of square
+        // System.out.println("Enter the side of square:");
+        // float a = sc.nextFloat();
+        // float area = a * a;
+        // System.out.println("Area of Square is:" + area);
+
+
+        // Question 3 Calculate total price of all item
+        // System.out.println("Enter the price of the pencil:");
+        // float pencil = sc.nextFloat();
+        // System.out.println("Enter price of pen:");
+        // float pen = sc.nextFloat();
+        // System.out.println("Enter the price of eraser:");
+        // float eraser = sc.nextFloat();
+        // float total = pencil + pen + eraser + (100/18);
+        // System.out.println("Total price is :" + total);
+
+
+        int $ = 4;
+        System.out.println($);
+    }
+}
