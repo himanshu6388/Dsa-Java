d371671b33136afd9a285237e6a5a3ab84760e6b
himanshu6388 <himanshu219181@gmail.com>
2025-04-22 07:52:20 +0530

    updated

--- Day01/Sum.java
+++ Day01/Sum.java
@@ -1,16 +1,51 @@
 package Day01;
 
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String args[]){
-        int a = 10;
-        int b = 5;
-        int sum = a + b;
-        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println("The sum of " + a + " and " + b + " is: " + sum);
 
         // -> Single line comment
 
         /* ->
          Multiline  Comment
          */
+
+
+         Scanner sc =  new Scanner(System.in);
+
+        //  String input =  sc.next();
+        //  System.out.println(input);     
+
+        // String name =  sc.nextLine();
+        // System.out.println(name);
+
+        // int num = sc.nextInt();
+        // System.out.println(num);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);  
+
+
+
+        // Sum of a and b
+        // System.out.println("Enter value of a:");
+        // int a = sc.nextInt();
+        // System.out.println("Enter value of b:");
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("The sum of " + a + " and " + b + " is:" + sum);
+
+        // Product of a and b
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
     }
 }
---
+++ Day01/typecast.java
@@ -0,0 +1,39 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class typecast {
+    public static void main(String args[]){
+        // int a = 25;
+        // long b= a;
+        // System.out.println(a);
+
+        Scanner sc = new Scanner(System.in);
+        //  float number =  sc.nextInt();
+        //  System.out.println(number);
+
+        // float a = 25.43f;
+        // int b = (int)a;
+        // System.out.println(b);
+
+        // char ch = 'a';
+        // int num =  ch;
+        // System.out.println(num);
+
+
+        // char a = 'a';
+        // char b = 'b';
+        // System.out.println((int)(b));
+        // System.out.println((int)(a));
+        // System.out.println(b-a);
+
+        int  a = 10;
+        float b = 20.35f;
+        long c = 25;
+        double d = 30;
+        double ans = a + b + c+d;
+        System.out.println(ans);
+
+    }
+    
+}
