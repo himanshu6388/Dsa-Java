dc82553e9c0b26abc5be691ab88eb28cb75164e5
himanshu6388 <himanshu219181@gmail.com>
2025-04-25 08:53:23 +0530

    Factorail Calculation

---
+++ functionMethod/fact.java
@@ -0,0 +1,23 @@
+package functionMethod;
+
+import java.util.Scanner;
+
+public class fact {
+
+    public static  int factorial(int n){
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact = fact*i;
+        }
+        return fact;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number to calculate Factorail:");
+        int n = sc.nextInt();
+        int fact = factorial(n);
+        System.out.println("Factorial is:" +fact);
+    }
+    
+}
---
+++ functionMethod/one.java
@@ -0,0 +1,47 @@
+package functionMethod;
+
+import java.util.Scanner;
+
+public class one {
+
+    public static void swap(int a,int b){
+        // swap
+        int temp = a;
+        a = b;
+        b = temp;
+        
+    }
+
+    public static void Greet(){
+        System.out.println("Hello World");
+    }
+    // Calculate Sum of two nuber function
+    public static void calculateSum(int a, int b){
+        int sum  = a+b;
+        System.out.println("Sum is:" +sum);
+        
+        // int sum = a + b;
+        // System.out.println("Sum is:" + sum);
+    }
+    public static void main(String args[]){
+        
+        // Greet();  // function call
+        // Greet();  //function call
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Value of a:");
+        // int a = sc.nextInt();
+        // System.out.println("Enter value of b:");
+        // int b = sc.nextInt();
+        // calculateSum(a, b);   //argumennts or actural parameters
+
+
+        // CAll by value
+        int a= 4;
+        int b = 10;
+        swap(a,b);
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+        
+    }
+}
