82aad3783e88b1b741a650fe2508551f7e74bb5e
himanshu6388 <himanshu219181@gmail.com>
2025-05-02 08:25:02 +0530

    Array Started

---
+++ day08Array/ar.java
@@ -0,0 +1,11 @@
+package day08Array;
+
+public class ar {
+    
+    public static void main(String args[]){
+
+
+        // array - > List of  Element of the same type placed in a contiguous memory location
+        
+    }
+}
