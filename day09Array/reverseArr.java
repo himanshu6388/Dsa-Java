db796ad5bd0a96361e3fbdc5c14372f97e910059
himanshu6388 <himanshu219181@gmail.com>
2025-05-05 20:23:15 +0530

    Make Consistency

--- day09Array/reverseArr.java
+++ day09Array/reverseArr.java
@@ -5,10 +5,11 @@ public class reverseArr {
 // Rverse the  array
 public static int revArray(int arr[]){
     for(int i=0;i<arr.length;i++){
-        for(int  j=arr.length-1;j<=i;J++){
+        for(int j=arr.length-1;j<=i;j++){
 
         }
     }
+    return 0;
 }
 
 
