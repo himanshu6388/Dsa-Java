02bfb7aa85240bbf03c38a2f80583c969a9057ac
himanshu6388 <himanshu219181@gmail.com>
2025-05-14 09:55:50 +0530

    updated

--- twoDarray/diagonalSum.java
+++ twoDarray/diagonalSum.java
@@ -4,26 +4,27 @@ public class diagonalSum {
     
     public static int diagoSum(int matrix[][]){
         int  sum = 0;
-    //    for(int i=0;i<matrix.length;i++){
-    //     for(int j=0;j<matrix[0].length;j++){
-    //         if(i == j){
-    //             sum += matrix[i][j];
-    //         }else if(i+j == matrix.length-1){
-    //             sum += matrix[i][j];
+    //     for(int i=0;i<matrix.length;i++){
+    //         for(int j=0;j<matrix[0].length;j++){
+    //             if(i==j){
+    //                 sum += matrix[i][j];;
+    //             }else if(i+j == matrix.length-1){
+    //                 sum += matrix[i][j];
+    //             }
     //         }
     //     }
-    //    }
-    //    return sum;
-
+    //     return sum;
+    // }
+    
     for(int i=0;i<matrix.length;i++){
+    
         sum += matrix[i][i];
 
         if(i != matrix.length-i-1)
-         sum += matrix[i][matrix.length-i-1];
+        sum +=  matrix[i][matrix.length-i-1];
     }
-
     return sum;
-    }
+}
 
     public static void main(String args[]){
         int matrix[][] = {
