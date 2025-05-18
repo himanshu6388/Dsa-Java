072719581e276dc9850a2ccff8ec9b7b19ca7a5c
himanshu6388 <himanshu219181@gmail.com>
2025-05-18 07:19:04 +0530

    String updated

---
+++ Strings/str.java
@@ -0,0 +1,24 @@
+package Strings;
+
+import java.util.*;
+
+public class str {
+    
+
+    public static void main(String args[]){
+        // char arr[] = {'a','b','c','d','e'};
+
+        // String str = "abcd";
+        // String str2 = new  String("xyz");
+
+
+        //String are IMMUTABLE
+
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        // name = sc.next();
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+}
