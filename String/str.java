46d41e424066eaeeda03efc8396ceedf2d1618d6
himanshu6388 <himanshu219181@gmail.com>
2025-05-18 07:13:22 +0530

    String  Started

---
+++ String/str.java
@@ -0,0 +1,15 @@
+package String;
+
+public class str {
+    
+
+    public static void main(String args[]){
+        char arr[] = {'a','b','c','d','e'};
+
+        String str = "abcd";
+        String str2 = new  String("xyz");
+
+
+        //String are IMMUTABLE
+    }
+}
