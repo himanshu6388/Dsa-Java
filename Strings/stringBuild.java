1acb9287438044b41805da2e6664767b0b8f3f88
himanshu6388 <himanshu219181@gmail.com>
2025-05-28 09:24:03 +0530

    Learn String Builder  and solve question

---
+++ Strings/stringBuild.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class stringBuild {
+    
+   
+
+    public static void main(String[] args) {
+        
+         StringBuilder  sb = new StringBuilder("");
+    for(char ch= 'a'; ch<='z';ch++){
+        sb.append(ch);
+    }
+    System.out.println(sb);
+    }
+}
